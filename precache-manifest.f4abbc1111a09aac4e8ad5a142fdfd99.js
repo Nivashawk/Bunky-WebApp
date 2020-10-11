5184dafa8b4ff906a3437693405d2fd0d28f63da
Nivas <nivasnivi67@gmail.com>
2020-10-12 04:11:42 +0530

    Updates

---
+++ precache-manifest.f4abbc1111a09aac4e8ad5a142fdfd99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fd12e8ff707a8e643dbf85161c17c3",
+    "url": "/Bunky-WebApp/index.html"
+  },
+  {
+    "revision": "9b030c889097aea4bcea",
+    "url": "/Bunky-WebApp/static/js/2.5ca9c8e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bunky-WebApp/static/js/2.5ca9c8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509f504794b90ca9433b",
+    "url": "/Bunky-WebApp/static/js/main.c6a9382f.chunk.js"
+  },
+  {
+    "revision": "92fb2db6dbbb567ef4f5",
+    "url": "/Bunky-WebApp/static/js/runtime-main.bf7b959e.js"
+  }
+]);
---
+++ static/js/main.c6a9382f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbunky=this.webpackJsonpbunky||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),u=t.n(r),a=t(2);var c=function(){return u.a.createElement("div",null,"welcome bunky")};t.n(a).a.render(u.a.createElement(u.a.StrictMode,null,u.a.createElement(c,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.c6a9382f.chunk.js.map
